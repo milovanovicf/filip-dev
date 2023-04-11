2f8e355a347dc0901ed042d71167b27c47d57d87
Filip <milovanovicfilip007@gmail.com>
2023-04-11 21:03:28 +0200

    Added Footer Section

---
+++ src/components/Footer.vue
@@ -0,0 +1,93 @@
+<template>
+  <div class="footer">
+    <div class="rights">
+      <p>© 2023 Filip Milovanovic <br />All rights reserved.</p>
+    </div>
+    <div class="email">
+      <p class="title">Email</p>
+      <a href="mailto:milovanovicfilip007@gmail.com"
+        >milovanovicfilip007@gmail.com</a
+      >
+    </div>
+    <div class="location">
+      <p class="title">Location</p>
+      <p>Belgrade, Serbia</p>
+    </div>
+    <div class="socials">
+      <p class="title">Social</p>
+      <ul>
+        <li><a href="#">LinkedIn</a></li>
+        <li><a href="#">Instagram</a></li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.footer {
+  height: 20rem;
+  margin: 10rem 15rem 0 15rem;
+  color: #fff;
+  display: flex;
+  justify-content: space-between;
+
+  .title {
+    font-size: 1.4rem;
+    text-transform: uppercase;
+    padding-bottom: 0.8rem;
+  }
+
+  a {
+    font-size: 2rem;
+    text-decoration: none;
+    color: #fff;
+  }
+
+  p {
+    font-size: 1.8rem;
+  }
+
+  li {
+    padding-bottom: 0.3rem;
+  }
+}
+
+@media only screen and (max-width: 1200px) {
+  .footer {
+    margin: 10rem 10rem 0 10rem;
+  }
+}
+
+@media only screen and (max-width: 1000px) {
+  .footer {
+    a {
+      font-size: 1.5rem;
+    }
+
+    p {
+      font-size: 1.5rem;
+    }
+  }
+}
+
+@media only screen and (max-width: 850px) {
+  .footer {
+    flex-direction: column;
+    margin: 5rem 5rem 0 5rem;
+    height: auto;
+
+    .rights,
+    .email,
+    .location,
+    .socials {
+      padding-bottom: 2rem;
+    }
+  }
+
+  @media only screen and (max-width: 1200px) {
+    .footer {
+      margin: 5rem 2rem 0 2rem;
+    }
+  }
+}
+</style>
