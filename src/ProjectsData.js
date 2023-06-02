8968c258d5131f90f8770eedd8bc16227583fa67
Filip <filip.milovanovic007@gmail.com>
2023-06-03 01:29:55 +0200

    added data for project

--- src/ProjectsData.js
+++ src/ProjectsData.js
@@ -77,36 +77,63 @@ const projectsData = [
   {
     id: 2,
     name: 'Learn English Bre!',
+    slug: 'learn-english-bre',
     overview:
       'Online English language learning platform for improving overall language skills for speaking, writing and most importantly understanding.',
     goals:
       'Help Serbo-Croatian speaking people to learn Enlgish faster and easier.',
-    slug: 'learn-english-bre',
     thumbnail: new URL(
       `/src/assets/images/learnEnglishBre/Preview.png`,
       import.meta.url
     ),
     platform: 'Desktop',
     technologies: ['Figma'],
+    features: [
+      {
+        id: 1,
+        text: 'After a quick sign up you will be redirected to the home page where you can select a course, track progress of your courses, chat with friends and more!',
+        image: new URL(
+          `/src/assets/images/learnEnglishBre/MainPage.png`,
+          import.meta.url
+        ),
+      },
+      {
+        id: 2,
+        text: 'Variety of different courses to choose from based on your requirements and skill level.',
+        image: new URL(
+          `/src/assets/images/learnEnglishBre/feature1.png`,
+          import.meta.url
+        ),
+      },
+      {
+        id: 3,
+        text: 'Our Libary contains a large amount of extra content like videos, dictionary and more! ',
+        image: new URL(
+          `/src/assets/images/learnEnglishBre/feature2.png`,
+          import.meta.url
+        ),
+      },
+      {
+        id: 4,
+        text: 'Grab a friend a challenge them in our daily chalanges and see who has the better knowlegde about certain topics.',
+        image: new URL(
+          `/src/assets/images/learnEnglishBre/feature3.png`,
+          import.meta.url
+        ),
+      },
+    ],
     content: {
       mainImage: new URL(
         `/src/assets/images/learnEnglishBre/MainImage.png`,
         import.meta.url
       ),
-      secondary1: new URL(
-        `/src/assets/images/learnEnglishBre/Secondary1.png`,
+      lastImage: new URL(
+        `/src/assets/images/learnEnglishBre/signup.png`,
         import.meta.url
       ),
-      secondary2: new URL(
-        `/src/assets/images/chatter/Secondary2.png`,
-        import.meta.url
-      ),
-      secondary3: new URL(
-        `/src/assets/images/chatter/Secondary3.png`,
-        import.meta.url
-      ),
-      secondary4: new URL(
-        `/src/assets/images/chatter/Secondary4.png`,
+
+      lastImageMobile: new URL(
+        `/src/assets/images/learnEnglishBre/signupMobile.png`,
         import.meta.url
       ),
       nextProject: new URL(
